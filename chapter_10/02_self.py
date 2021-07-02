c950ebe55f7378295ea57a186ca33c9ebaf4293a
Md.Dulal Miah <dulalmiah70112@gmail.com>
2021-07-02 16:56:09 +0600

    start oop of python

---
+++ chapter_10/02_self.py
@@ -0,0 +1,11 @@
+class Employee:
+    company = "Google"
+    def getSalary(self):
+        print(f"Your salary is {self.salary}k and company is {self.company}")
+        
+dulal = Employee()
+dulal.salary = 100
+dulal.getSalary()
+
+#class attribute
+#instance attribute > class attribute
---
+++ chapter_10/employee.py
@@ -0,0 +1,9 @@
+class Employee:
+    company = 'Programming-Hero'
+
+Dulal = Employee()
+# print(Dulal.company)
+# Employee.company = 'Google'
+# print(Dulal.company)
+Dulal.salary = '50k'
+print(Dulal.salary)
